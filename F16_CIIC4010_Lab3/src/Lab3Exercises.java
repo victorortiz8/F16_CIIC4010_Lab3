369a1c88c0455e77d691a58ab57c317121c563a8
4010a4 <4010a4@granada.ece.uprm.edu>
2016-08-31 08:46:45 -0400

    Changed the title of the frame as well as it's location and size.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Victor Ortiz");
+		JFrame myFrame = new JFrame("Victor M. Ortiz");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(850, 425);
 		myFrame.setSize(200, 200);
